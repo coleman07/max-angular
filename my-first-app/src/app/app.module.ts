4c42f154d76f733db9605584dee70818c5652fd4
Sean Coleman <coleman07@gmail.com>
2020-12-13 16:03:55 -0700

    components finished

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],

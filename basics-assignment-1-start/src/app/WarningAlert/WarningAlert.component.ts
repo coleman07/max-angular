632cc5607cd9063444d744daba56d15b266aca78
Sean Coleman <coleman07@gmail.com>
2020-12-13 16:52:16 -0700

    Exercise one solution

---
+++ basics-assignment-1-start/src/app/WarningAlert/WarningAlert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-warningalert",
+  templateUrl: "./WarningAlert.component.html",
+  styleUrls: ["./WarningAlert.component.css"],
+})
+export class WarningAlertComponent {}
